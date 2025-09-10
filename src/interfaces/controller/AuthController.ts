b94bdcef20c4641ef417979e436d3b2a6937529c
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-09-10 14:37:31 +0200

    feat: add field validation in `AuthController` and new error handlers

--- src/interfaces/controller/AuthController.ts
+++ src/interfaces/controller/AuthController.ts
@@ -1,6 +1,8 @@
 import { AuthService } from "../../domain/ports/AuthService";
 import { Request, Response } from "express";
 import { InvalidCredentialsError } from "../../domain/errors/errors";
+import { FieldRequiredError } from "../api/errors/FieldRequired";
+import { InvalidRequest } from "../api/errors/InvalidRequest";
 
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -8,13 +10,20 @@ export class AuthController {
   login = async (request: Request, response: Response): Promise<Response> => {
     try {
       const { username, password } = request.body;
-      const tokens = await this.authService.login(username, password);
-      return response.status(200).json(tokens);
-    } catch (error: unknown) {
-      if (error instanceof InvalidCredentialsError) {
-        return response.status(401).json({ message: error.message });
+      if (!username) {
+        return response.status(400).json(FieldRequiredError("Username"));
       }
-      return response.status(500).json({ message: "Internal server error" });
+
+      if (!password) {
+        return response.status(400).json(FieldRequiredError("Password"));
+      }
+      const token = await this.authService.login(username, password);
+      return response.status(200).json(token);
+    } catch (error) {
+      if (error instanceof InvalidCredentialsError) {
+        return response.status(402).json({ message: error.message });
+      }
+      return response.status(400).json(InvalidRequest);
     }
   };
 }
