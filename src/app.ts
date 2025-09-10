c5146e0ebc132d4cf1dd3fe9cee04ed09fec79c8
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-09-10 11:55:56 +0200

    feat: add authentication middleware and server initialization logic

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,21 @@
-import dotenv from "dotenv";
-dotenv.config();
+import "dotenv/config";
+import express from "express";
+import routes from "./interfaces/routes/routes";
+
+const app = express();
+app.use(express.json());
+
+const startServer = async () => {
+  try {
+    app.use(routes);
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server on http://localhost:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
