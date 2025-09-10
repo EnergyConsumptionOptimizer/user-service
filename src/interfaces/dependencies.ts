05d1f8f8f0a772a61ddd62feaeb977acd72414b7
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-09-10 14:12:09 +0200

    feat: add `Routes` module and integrate with `app.js`

---
+++ src/interfaces/dependencies.ts
@@ -0,0 +1,21 @@
+import { AuthController } from "./controller/AuthController";
+import { AuthServiceImpl } from "../application/AuthService";
+import { InMemoryUserRepository } from "../test/storage/InMemoryUserRepository";
+import { JWTService } from "../application/JWTService";
+import { UserController } from "./controller/UserController";
+import { UserServiceImpl } from "../application/UserServiceImpl";
+import { AuthMiddleware } from "./middleware/AuthMiddleware";
+
+// ===== Services =====
+const userRepository = new InMemoryUserRepository();
+const jwtService = new JWTService();
+
+const authService = new AuthServiceImpl(userRepository, jwtService);
+const userService = new UserServiceImpl(userRepository);
+
+// ===== Controllers =====
+export const authController = new AuthController(authService);
+export const userController = new UserController(userService);
+
+// ===== Middleware =====
+export const authMiddleware = new AuthMiddleware(jwtService);
