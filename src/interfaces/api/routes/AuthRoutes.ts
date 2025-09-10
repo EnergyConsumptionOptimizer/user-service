c59ccbbd40d9f9fe53ad44e6df02c000fcbd85e2
rares vasiliu <rv.vasiliu@gmail.com>
2025-09-10 21:46:45 +0200

    feat: add logout route
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/interfaces/api/routes/AuthRoutes.ts
+++ src/interfaces/api/routes/AuthRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { authController } from "../dependencies";
+import { authController, authMiddleware } from "../dependencies";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", authController.login);
+
+authRouter.post("/logout", authMiddleware.authenticate, authController.logout);
 
 authRouter.post("/refresh", authController.refresh);
 
