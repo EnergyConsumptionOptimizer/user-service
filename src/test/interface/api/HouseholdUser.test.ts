9029686f76bd8cbb916332a491f0f119c49eed21
rares vasiliu <rv.vasiliu@gmail.com>
2025-09-13 18:15:59 +0200

    test: improve household user test

--- src/test/interface/api/HouseholdUser.test.ts
+++ src/test/interface/api/HouseholdUser.test.ts
@@ -17,25 +17,47 @@ import { userRepository, userService } from "../dependencies";
 
 describe("api/household-users/", () => {
   const url = "/api/household-users";
-  const buildIDUrl = (id: UserID) => `${url}/${id.value}`;
 
   let admin: User;
-  let householdUserMark: User;
-  let householdUserDavid: User;
-
   let adminAccessToken: string;
+
+  let householdUserMark: User;
   let markAccessToken: string;
 
-  beforeAll(async () => {
-    const adminHashedPassword = await bcrypt.hash(mockAdminUser.password, 10);
+  let householdUserDavid: User;
 
+  const accessTokenFromLoginRequest = async (
+    username: string,
+    password: string,
+  ) => {
+    const res = await request(app).post("/api/auth/login").send({
+      username,
+      password,
+    });
+
+    return "Bearer " + res.body.accessToken;
+  };
+
+  const setAuth = (token?: string) => token ?? "";
+
+  beforeAll(async () => {
     admin = await userRepository.addNewHouseholdUser({
       ...mockAdminUser,
-      password: adminHashedPassword,
+      password: await bcrypt.hash(mockAdminUser.password, 10),
     });
+
+    adminAccessToken = await accessTokenFromLoginRequest(
+      admin.username,
+      mockAdminUser.password,
+    );
 
     householdUserMark = await userService.createHouseholdUser(
       mockHouseholdUserMark.username,
+      mockHouseholdUserMark.password,
+    );
+
+    markAccessToken = await accessTokenFromLoginRequest(
+      householdUserMark.username,
       mockHouseholdUserMark.password,
     );
 
@@ -43,35 +65,22 @@ describe("api/household-users/", () => {
       mockHouseholdUserDavid.username,
       mockHouseholdUserDavid.password,
     );
-
-    const authResponse = await request(app).post("/api/auth/login").send({
-      username: admin.username,
-      password: mockAdminUser.password,
-    });
-
-    adminAccessToken = "Bearer " + authResponse.body.accessToken;
-
-    const authResponseMark = await request(app).post("/api/auth/login").send({
-      username: mockHouseholdUserMark.username,
-      password: mockHouseholdUserMark.password,
-    });
-
-    markAccessToken = "Bearer " + authResponseMark.body.accessToken;
   });
 
-  describe("GET /", () => {
-    it("should return the list of household users when the admin requests it", async () => {
-      const response = await request(app)
-        .get(url)
-        .set("Authorization", adminAccessToken);
+  describe("GET / - List household users", () => {
+    const getAllRequest = async (token?: string) =>
+      request(app).get(url).set("Authorization", setAuth(token));
+
+    it("should return household users list when admin requests it", async () => {
+      const response = await getAllRequest(adminAccessToken);
 
       expect(response.status).toBe(200);
 
-      const usernames = response.body["household-users"].map(
-        (u: User) => u.username,
-      );
+      const usernames = response.body["household-users"]
+        .map((u: User) => u.username)
+        .sort();
 
-      expect(usernames.sort()).toEqual(
+      expect(usernames).toEqual(
         [
           mockHouseholdUserMark.username,
           mockHouseholdUserDavid.username,
@@ -79,31 +88,43 @@ describe("api/household-users/", () => {
       );
     });
 
-    it("should return 401 when no auth is provided", async () => {
-      const response = await request(app).get(url);
+    it("should return 401 when no authentication is provided", async () => {
+      const response = await getAllRequest();
 
       expect(response.status).toBe(401);
     });
 
-    it("should return 403 when a non-admin user tries to access", async () => {
-      const response = await request(app)
-        .get(url)
-        .set("Authorization", markAccessToken);
+    it("should return 403 when non-admin user tries to access", async () => {
+      const response = await getAllRequest(markAccessToken);
 
       expect(response.status).toBe(403);
     });
   });
 
-  describe("POST /", () => {
-    it("should add a new household user when valid username and password are provided by the admin", async () => {
-      const newUser = mockHouseholdUserEmma;
-      const response = await request(app)
+  describe("POST / - Create household user", () => {
+    const createNewHouseholdUserRequest = async (
+      user?: { username: string; password: string },
+      token?: string,
+    ) =>
+      request(app)
         .post(url)
-        .send({
-          username: newUser.username,
-          password: newUser.password,
-        })
-        .set("Authorization", adminAccessToken);
+        .send(
+          user
+            ? {
+                username: user.username,
+                password: user.password,
+              }
+            : {},
+        )
+        .set("Authorization", setAuth(token));
+
+    it("should create household user when admin provides valid username and password", async () => {
+      const newUser = mockHouseholdUserEmma;
+
+      const response = await createNewHouseholdUserRequest(
+        mockHouseholdUserEmma,
+        adminAccessToken,
+      );
 
       expect(response.status).toBe(201);
 
@@ -111,171 +132,190 @@ describe("api/household-users/", () => {
     });
 
     it("should return 400 status code when no data is provided", async () => {
-      const response = await request(app)
-        .post(url)
-        .send({})
-        .set("Authorization", adminAccessToken);
+      const response = await createNewHouseholdUserRequest(
+        undefined,
+        adminAccessToken,
+      );
 
       expect(response.status).toBe(400);
     });
 
-    it("should return 401 when no auth is provided", async () => {
-      const response = await request(app).post(url).send({
-        username: "username",
-        password: "password",
-      });
-
-      expect(response.status).toBe(401);
-    });
-
-    it("should return 403 when an household user tries to access", async () => {
-      const response = await request(app)
-        .post(url)
-        .set("Authorization", markAccessToken)
-        .send({
-          username: "username",
-          password: "password",
-        });
-
-      expect(response.status).toBe(403);
-    });
-
-    it("should return 409 status code when the admin tries to create an account with an already existing username", async () => {
-      const response = await request(app)
-        .post(url)
-        .send({
-          username: householdUserMark.username,
-          password: "password",
-        })
-        .set("Authorization", adminAccessToken);
-
-      expect(response.status).toBe(409);
-    });
-  });
-
-  describe("PUT /:id/username", () => {
-    const buildChangeUsernameUrl = (id: UserID) => buildIDUrl(id) + "/username";
-
-    it("should update the username of an existing household user by the admin", async () => {
-      const user = await userService.createHouseholdUser("alex", "password");
-
-      const response = await request(app)
-        .put(buildChangeUsernameUrl(user.id))
-        .send({
-          username: "riley",
-        })
-        .set("Authorization", adminAccessToken);
-
-      expect(response.status).toBe(204);
-    });
-
-    it("should update the username of a household user by the same household user who owns the account", async () => {
-      const response = await request(app)
-        .put(buildChangeUsernameUrl(householdUserMark.id))
-        .send({
-          username: "mark01",
-        })
-        .set("Authorization", markAccessToken);
-
-      expect(response.status).toBe(204);
-    });
-
-    it("should return 401 when no auth is provided", async () => {
-      const response = await request(app)
-        .put(buildChangeUsernameUrl(householdUserMark.id))
-        .send({
-          username: "username",
-        });
-
-      expect(response.status).toBe(401);
-    });
-
-    it("should return 403 when a household user tries to update another household user's username", async () => {
-      const response = await request(app)
-        .put(buildChangeUsernameUrl(householdUserDavid.id))
-        .set("Authorization", markAccessToken)
-        .send({
-          username: "alex",
-        });
-
-      expect(response.status).toBe(403);
-    });
-
-    it("should return 404 when trying to update admin username", async () => {
-      const response = await request(app)
-        .put(buildChangeUsernameUrl(admin.id))
-        .send({
-          username: "username",
-        })
-        .set("Authorization", adminAccessToken);
-
-      expect(response.status).toBe(404);
-    });
-
-    it("should return 404 when household user doesn't exists", async () => {
-      const response = await request(app)
-        .put(buildChangeUsernameUrl({ value: uuidv4() }))
-        .send({
-          username: "username",
-        })
-        .set("Authorization", adminAccessToken);
-
-      expect(response.status).toBe(404);
-    });
-
-    it("should return 409 when trying to use an already existing username", async () => {
-      const user = await userService.createHouseholdUser("u1", "password");
-
-      const response = await request(app)
-        .put(buildChangeUsernameUrl(householdUserDavid.id))
-        .send({
-          username: user.username,
-        })
-        .set("Authorization", adminAccessToken);
-
-      expect(response.status).toBe(409);
-    });
-  });
-
-  describe("DELETE /:id/", () => {
-    it("should delete an existing household user by the admin", async () => {
-      const user = await userService.createHouseholdUser("alex", "password");
-
-      const response = await request(app)
-        .delete(buildIDUrl(user.id))
-        .set("Authorization", adminAccessToken);
-
-      expect(response.status).toBe(204);
-    });
-
-    it("should return 401 when no auth is provided", async () => {
-      const response = await request(app).delete(
-        buildIDUrl(householdUserMark.id),
+    it("should return 401 when no authentication is provided", async () => {
+      const response = await createNewHouseholdUserRequest(
+        mockHouseholdUserEmma,
       );
 
       expect(response.status).toBe(401);
     });
 
-    it("should return 403 an household user tries to access", async () => {
-      const response = await request(app)
-        .delete(buildIDUrl(householdUserMark.id))
-        .set("Authorization", markAccessToken);
+    it("should return 403 when household user tries to create account", async () => {
+      const response = await createNewHouseholdUserRequest(
+        mockHouseholdUserEmma,
+        markAccessToken,
+      );
 
       expect(response.status).toBe(403);
     });
 
-    it("should return 404 when trying to delete admin account", async () => {
-      const response = await request(app)
-        .delete(buildIDUrl(admin.id))
-        .set("Authorization", adminAccessToken);
+    it("should return 409 when username already exists", async () => {
+      const response = await createNewHouseholdUserRequest(
+        mockHouseholdUserMark,
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(409);
+    });
+  });
+
+  describe("PUT /:id/username - Update username", () => {
+    const changeUsernameRequest = async (
+      id: UserID,
+      username?: string,
+      token?: string,
+    ) =>
+      request(app)
+        .put(url + "/" + id.value + "/username")
+        .send(
+          username
+            ? {
+                username: username,
+              }
+            : {},
+        )
+        .set("Authorization", setAuth(token));
+
+    it("should allow admin to update any household user's username", async () => {
+      const user = await userService.createHouseholdUser("alex", "password");
+      const newUsername = "riley";
+
+      const response = await changeUsernameRequest(
+        user.id,
+        newUsername,
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(201);
+      expect(response.body["username"]).toBe(newUsername);
+    });
+
+    it("should update the username of a household user by the same household user who owns the account", async () => {
+      householdUserMark.username = "mark01";
+      const response = await changeUsernameRequest(
+        householdUserMark.id,
+        householdUserMark.username,
+        markAccessToken,
+      );
+
+      expect(response.status).toBe(201);
+      expect(response.body["username"]).toBe(householdUserMark.username);
+    });
+
+    it("should return 400 status code when no data is provided", async () => {
+      const response = await changeUsernameRequest(
+        householdUserMark.id,
+        undefined,
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(400);
+    });
+
+    it("should return 401 when no auth is provided", async () => {
+      const response = await changeUsernameRequest(
+        householdUserMark.id,
+        "username",
+      );
+
+      expect(response.status).toBe(401);
+    });
+
+    it("should return 403 when household user tries to update another user's username", async () => {
+      const response = await changeUsernameRequest(
+        householdUserDavid.id,
+        "username",
+        markAccessToken,
+      );
+
+      expect(response.status).toBe(403);
+    });
+
+    it("should return 404 when trying to update admin username", async () => {
+      const response = await changeUsernameRequest(
+        admin.id,
+        "username",
+        adminAccessToken,
+      );
 
       expect(response.status).toBe(404);
     });
 
     it("should return 404 when household user doesn't exists", async () => {
-      const response = await request(app)
-        .delete(buildIDUrl({ value: uuidv4() }))
-        .set("Authorization", adminAccessToken);
+      const response = await changeUsernameRequest(
+        { value: uuidv4() },
+        "username",
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(404);
+    });
+
+    it("should return 409 when trying to use existing username", async () => {
+      const user = await userService.createHouseholdUser("alex", "password");
+
+      const response = await changeUsernameRequest(
+        user.id,
+        householdUserMark.username,
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(409);
+    });
+  });
+
+  describe("DELETE /:id/ - should return 404 when user does not exist", () => {
+    const deleteHouseholdUserRequest = async (id: UserID, token?: string) =>
+      request(app)
+        .delete(url + "/" + id.value)
+        .set("Authorization", setAuth(token));
+
+    it("should allow admin to delete household user", async () => {
+      const response = await deleteHouseholdUserRequest(
+        householdUserDavid.id,
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(204);
+    });
+
+    it("should return 401 when no authentication is provided", async () => {
+      const response = await deleteHouseholdUserRequest(householdUserMark.id);
+
+      expect(response.status).toBe(401);
+    });
+
+    it("should return 403 when household user tries to delete account", async () => {
+      const response = await deleteHouseholdUserRequest(
+        householdUserMark.id,
+        markAccessToken,
+      );
+
+      expect(response.status).toBe(403);
+    });
+
+    it("should return 404 when trying to delete admin account", async () => {
+      const response = await deleteHouseholdUserRequest(
+        admin.id,
+        adminAccessToken,
+      );
+
+      expect(response.status).toBe(404);
+    });
+
+    it("should return 404 when user does not exist", async () => {
+      const response = await deleteHouseholdUserRequest(
+        { value: uuidv4() },
+        adminAccessToken,
+      );
 
       expect(response.status).toBe(404);
     });
