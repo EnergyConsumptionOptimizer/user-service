71726fa0a7e29565c1f98b354d0dc1b4374bd121
rares vasiliu <rv.vasiliu@gmail.com>
2025-10-31 09:16:22 +0100

    test: reactor JWTService to use variables in the constructor
    Signed-off-by: rares vasiliu <rv.vasiliu@gmail.com>

--- src/test/interface/dependencies.ts
+++ src/test/interface/dependencies.ts
@@ -7,20 +7,13 @@ import { UserController } from "../../interfaces/api/controllers/UserController"
 import { AuthMiddleware } from "../../interfaces/api/middleware/AuthMiddleware";
 import { router } from "../../interfaces/api/routes/router";
 
-export const JWT_SECRET = "test_secret_key";
-export const JWT_EXPIRES_IN = "1h";
-export const JWT_REFRESH_EXPIRES_IN = "7d";
 export const RESET_CODE = "secret_reset_code";
 
 // ===== Repository =====
 export const userRepository = new InMemoryUserRepository();
 
 // ===== Services =====
-export const jwtService = new JWTService(
-  JWT_SECRET,
-  JWT_EXPIRES_IN,
-  JWT_REFRESH_EXPIRES_IN,
-);
+export const jwtService = new JWTService();
 export const authService = new AuthServiceImpl(userRepository, jwtService);
 export const userService = new UserServiceImpl(userRepository, RESET_CODE);
 
