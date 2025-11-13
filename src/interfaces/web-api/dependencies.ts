815f9f86c0e573637be1f6763e3a66a96419278e
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-11-13 16:21:35 +0100

    feat: add dependency injection setup for web API

---
+++ src/interfaces/web-api/dependencies.ts
@@ -0,0 +1,19 @@
+import { MongooseUserRepository } from "@storage/mongo/MongooseUserRepository";
+import { JWTService } from "@application/JWTService";
+import { AuthServiceImpl } from "@application/AuthService";
+import { UserServiceImpl } from "@application/UserServiceImpl";
+import { router } from "./routes/router";
+
+export const userRepository = new MongooseUserRepository();
+
+// ===== Services =====
+export const jwtService = new JWTService();
+
+export const authService = new AuthServiceImpl(userRepository, jwtService);
+export const userService = new UserServiceImpl(
+  userRepository,
+  process.env.RESET_CODE || "1234",
+);
+
+// ===== Router =====
+export const apiRouter = router();
