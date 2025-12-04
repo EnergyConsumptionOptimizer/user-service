d138066df2ed760169b5977b7e09003c4f2e8136
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-04 23:23:39 +0100

    chore: integrate `cookie-parser` middleware into Express application

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { apiRouter } from "@interfaces/web-api/dependencies";
 import { errorsHandler } from "@interfaces/web-api/middleware/ErrorsMiddleware";
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(express.json());
 
