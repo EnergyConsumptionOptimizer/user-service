51b2eb4de16f901bd4f6cd6cb18328ac798c6cf1
SalvatoreBennici <salvatore.bennici@studio.unibo.it>
2025-12-19 14:59:59 +0100

    test: integrate `cookie-parser` middleware into test application

--- src/test/interfaces/app.ts
+++ src/test/interfaces/app.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 import { errorsHandler } from "@interfaces/web-api/middleware/ErrorsMiddleware";
 import { apiRouter } from "./dependencies";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(apiRouter);
 app.use(errorsHandler);
